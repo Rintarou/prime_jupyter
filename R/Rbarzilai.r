0573da60b7a21d9553bc965b1e7ba352f2e72cc7
Slaanaroth <corentin.marionneau@gmail.com>
2017-12-07 01:06:38 +0100

    Best function of them all

--- R/Rbarzilai.r
+++ R/Rbarzilai.r
@@ -3,35 +3,13 @@
 # This implementation is recursive
 
 
-cribleRec <- function(n,tab){
-  # Stopping condition, if we get to 1 we return the array as it was
-  if (n == 1)
-  {
-    return(tab)
-  }
-  # If we're not yet at 1, we remove all multiples of n from the array
-  else
-  {
-    # Copying the current state of the array to create the result array
-    newTab <- tab
-
-    # Iterating all numbers above n and removing them if they're multiples of n
-    for (i in n:max(tab)) {
-      if (i != n)
-      {
-        if (i%%n == 0)
-        {
-          newTab <- newTab[newTab != i]
-        }
-      }
-    }
-    # Calling the same function for n-1 on the treated array
-    return(cribleRec(n-1,newTab))
-  }
-}
-
-# Calling the recursive function for numbers from 1 to n
 crible <- function(n){
-  # Starting with n/2 because it is useless to start above
-  cribleRec(n/2,c(2:n))
+  # Initialising an array from 2 to n
+  t <- 2:n
+  # For each number between 2 and square root of n (because no need to go further)
+  for (i in 2:sqrt(n)) {
+    # We keep only the values not multiples of i (or i itself)
+    t = Filter(function (x) x%%i != 0 || x == i, t)
+  }
+  return(t)
 }
